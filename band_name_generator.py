e498eb768a30f392f70be97131d308641a43ad50
IonutHaita <ionut.haita93@gmail.com>
2024-08-24 02:54:27 +0300

    .

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the Band name generator!")
+name_of_city = input("What is the name of the city you grew up in?\n")
+name_of_pet = input("What's your pet's name?\n")
+print("Your band name would be " + name_of_city + " " + name_of_pet)
