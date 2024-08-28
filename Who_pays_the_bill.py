1490a607c96127659afe47afc2536bf35b33751e
IonutHaita <ionut.haita93@gmail.com>
2024-08-28 19:54:27 +0300

    .

---
+++ Who_pays_the_bill.py
@@ -0,0 +1,15 @@
+import random
+
+#states_of_America = ["Ohio", "Pennsylvania", "Delaware", "Idaho"]
+#print(states_of_America[2])
+
+#states_of_America.append("Washington")
+#print(states_of_America)
+
+friends = ["Alice", "John", "Mark", "Dick", "James", "Tony"]
+
+#rand_index = random.randint(0, 5)
+
+#print(friends[rand_index] + " will pay today's bill.")
+
+print(random.choice(friends) + " will pay the bill today.")
