454e24bce78def556611e3fb74794a229129d14b
IonutHaita <ionut.haita93@gmail.com>
2024-08-28 20:17:19 +0300

    .

--- Who_pays_the_bill.py
+++ Who_pays_the_bill.py
@@ -11,5 +11,5 @@ friends = ["Alice", "John", "Mark", "Dick", "James", "Tony"]
 #rand_index = random.randint(0, 5)
 
 #print(friends[rand_index] + " will pay today's bill.")
-
+print(friends[-2])
 print(random.choice(friends) + " will pay the bill today.")
