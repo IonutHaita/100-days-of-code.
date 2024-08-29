7df6e89d4baff51205f1983d66cf24a7e74af52b
IonutHaita <ionut.haita93@gmail.com>
2024-08-29 19:14:57 +0300

    .

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,6 +1,11 @@
 import random
 
 choice = "None"
+input_valid = False
+player = "Player wins, congratulations!"
+computer = "Computer wins, nice try!"
+winner1 = False
+winner2 = False
 
 def write_to_terminal(choice):
     Rock = False
@@ -45,16 +50,46 @@ def write_to_terminal(choice):
     ---.__(___)
     """)
         
+def check_input(choice):
+    global input_valid
+    input_valid = True if choice == "Rock" or choice == "Paper" or choice == "Scissors" else False
+
+def determine_winner(choice, random_selection):
+    global winner1, winner2
+    if choice == "Paper" and random_selection == "Rock":
+        winner1 = True 
+    elif choice == "Rock"  and random_selection == "Scissors":
+        winner1 = True 
+    elif choice == "Scissors" and random_selection == "Paper":
+        winner1 = True 
+    if choice == "Rock" and random_selection == "Paper":
+        winner1 = True 
+    elif choice == "Scissors"  and random_selection == "Rock":
+        winner2 = True 
+    elif choice == "Paper" and random_selection == "Scissors":
+        winner1 = True 
+
 print("Welcome to Rock, Paper, Scissors!")
 
-try:
-    choice = input("To play the game write your selection(Rock, Paper or Scissors):\n")
-except ValueError:
-    print("Invalid input! Try again!")
-print("Your selection:")
-write_to_terminal(choice)
+choice = input("To play the game write your selection(Rock, Paper or Scissors):\n")
 
-options = ["Rock", "Paper", "Scissors"]
-random_selection = random.choice(options)
-print("Computer's choice:")
-write_to_terminal(random_selection)
+check_input(choice)
+
+if input_valid:
+    print("Your selection:")
+    write_to_terminal(choice)
+
+    options = ["Rock", "Paper", "Scissors"]
+    random_selection = random.choice(options)
+    print("Computer's choice:")
+    write_to_terminal(random_selection)
+
+    determine_winner(choice, random_selection)
+    if winner1:
+        print(player)
+    elif winner2:
+        print(computer)
+    else:
+        print("It's a draw! Better luck next time!")
+else:
+    print("Wrong input! Check for misspels and try again!")
