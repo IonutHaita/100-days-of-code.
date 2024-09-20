9deed56f30f820be5b5ca1bf7fffda67d448a1c4
IonutHaita <ionut.haita93@gmail.com>
2024-09-20 19:35:06 +0300

    .

--- 8th_day.py
+++ 8th_day.py
@@ -1,5 +1,13 @@
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How are you today {name} ?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How are you today {name} ?")
 
-greet_with_name("Angela")
+# greet_with_name("Angela")
+
+def time_in_weeks(age):
+    time_left = 90 - age
+    time_in_weeks = time_left * 52
+    t_left = str(time_in_weeks)
+    print("You have " + t_left + " weeks left.")
+
+time_in_weeks(56)
